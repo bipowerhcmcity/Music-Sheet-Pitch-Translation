5d5d98bd08ead680052f7721a147dbf59583f582
bipowerhcmcity <lmht772000@gmail.com>
2021-07-10 19:17:17 +0700

    test tranpose

--- NotesTranslation.py
+++ NotesTranslation.py
@@ -4,7 +4,7 @@ notes_height_1 = ['E5', 'F5', 'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F6','G6']
 
 
 notes_height_BASS = ['G3', 'F3', 'E3', 'D3', 'C3', 'B2', 'A2','G2', 'F2', 'E2', 'D2', 'C2', 'B1', 'A1','G1', 'F1', 'E1']#, 'B', 'A', 'G', 'F','E', 'D', 'C', 'B', 'A', 'G', 'F']
-notes_height_BASS_1 = ['G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4']
+notes_height_BASS_1 = ['G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4','A4', 'B4']
 
 
 
